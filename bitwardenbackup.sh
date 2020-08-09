b8355ef63fd9a7c303319ce5adaaca51ebbd1fd1
Broizter <nthorild@gmail.com>
2020-08-09 23:09:25 +0200

    Significant code cleanup

--- bitwardenbackup.sh
+++ bitwardenbackup.sh
@@ -8,36 +8,40 @@ BW_BINARY=/path/to/bitwarden-cli/bin
 
 ############ END OF CONFIGURATION ################
 
+
+# Check if the output directory exists
 if ! [ -d $OUTPUTFOLDER ]; then
         echo "Output folder doesn't exist." 1>&2
         exit 1
 fi
 
+# Check if the Bitwarden CLI binary exists
 if ! [ -f $BW_BINARY ]; then
         echo "Bitwarden binary cannot be found. Check your settings." 1>&2
         exit 1
 fi
 
-if ! $BW_BINARY login --check; then
-	exit 1
-fi
+# Check if the user is logged into Bitwarden
+$BW_BINARY login --check > /dev/null || exit 1
 
 # Variables used later in script. Not meant to be changed.
-SESSIONS=$($BW_BINARY unlock --raw $PASSWORD)
-PARENTS=$($BW_BINARY list items --session $SESSIONS --pretty | jq '.[] | select(.attachments)' | grep -o -P '(?<="id": ").*(?=",)' | sed -n -e '/-/{p;n;}')
+SESSION=$($BW_BINARY unlock --raw $PASSWORD)
+PARENTS=$($BW_BINARY list items --session $SESSION | jq -r '.[] | select(.attachments) | .id')
 ATTACHFOLDER=$OUTPUTFOLDER/attachments/
 JSONFILE=$OUTPUTFOLDER/vault.json
 EXIT=0
 
-$BW_BINARY sync --session $SESSIONS || EXIT=$?
-$BW_BINARY export $PASSWORD --format json --session $SESSIONS --output $JSONFILE || EXIT=$?
+# Detect changes to your Bitwarden vault and export passwords to json file
+$BW_BINARY sync --session $SESSION || EXIT=$?
+$BW_BINARY export $PASSWORD --format json --session $SESSION --output $JSONFILE || EXIT=$?
 
+# Goes through any item that contains attachments and downloads them
 for P in $PARENTS; do
-        ATTACH=$(echo $P $($BW_BINARY get item $P --pretty --session $SESSIONS | jq .attachments | grep -o -P '(?<="id": ").*(?=",)') | awk '{$1=""; print $0}')
+        ATTACH=$($BW_BINARY get item $P --session $SESSION | jq -r .attachments[].id)
         for A in $ATTACH; do
-                $BW_BINARY get attachment $A --itemid $P --session $SESSIONS --output $ATTACHFOLDER || EXIT=$?
-                sleep 1
+                $BW_BINARY get attachment $A --itemid $P --session $SESSION --output $ATTACHFOLDER || EXIT=$?
         done
 done
 
+# Exits with zero code 0 unless anything went wrong
 exit $EXIT
