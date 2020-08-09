dbc838b5f0298a323525074039752a3aed7981bf
Broizter <nthorild@gmail.com>
2020-08-09 20:08:13 +0200

    Added some basic error handling

--- bitwardenbackup.sh
+++ bitwardenbackup.sh
@@ -8,23 +8,36 @@ BW_BINARY=/path/to/bitwarden-cli/bin
 
 ############ END OF CONFIGURATION ################
 
+if ! [ -d $OUTPUTFOLDER ]; then
+        echo "Output folder doesn't exist." 1>&2
+        exit 1
+fi
+
+if ! [ -f $BW_BINARY ]; then
+        echo "Bitwarden binary cannot be found. Check your settings." 1>&2
+        exit 1
+fi
+
+if ! $BW_BINARY login --check; then
+	exit 1
+fi
+
 # Variables used later in script. Not meant to be changed.
 SESSIONS=$($BW_BINARY unlock --raw $PASSWORD)
 PARENTS=$($BW_BINARY list items --session $SESSIONS --pretty | jq '.[] | select(.attachments)' | grep -o -P '(?<="id": ").*(?=",)' | sed -n -e '/-/{p;n;}')
 ATTACHFOLDER=$OUTPUTFOLDER/attachments/
 JSONFILE=$OUTPUTFOLDER/vault.json
+EXIT=0
 
-# Sync Bitwarden to detect any changes done to the vault before exporting
-$BW_BINARY sync --session $SESSIONS
+$BW_BINARY sync --session $SESSIONS || EXIT=$?
+$BW_BINARY export $PASSWORD --format json --session $SESSIONS --output $JSONFILE || EXIT=$?
 
-# Backup vault (excluding attachments)
-$BW_BINARY export $PASSWORD --format json --session $SESSIONS --output $JSONFILE
-
-# Search for items in the vault that contain attachments, then download all attachments in the vault
 for P in $PARENTS; do
-	ATTACH=$(echo $P $($BW_BINARY get item $P --pretty --session $SESSIONS | jq .attachments | grep -o -P '(?<="id": ").*(?=",)') | awk '{$1=""; print $0}')
-	for A in $ATTACH; do
-		$BW_BINARY get attachment $A --itemid $P --session $SESSIONS --output $ATTACHFOLDER
-		sleep 1
-	done
+        ATTACH=$(echo $P $($BW_BINARY get item $P --pretty --session $SESSIONS | jq .attachments | grep -o -P '(?<="id": ").*(?=",)') | awk '{$1=""; print $0}')
+        for A in $ATTACH; do
+                $BW_BINARY get attachment $A --itemid $P --session $SESSIONS --output $ATTACHFOLDER || EXIT=$?
+                sleep 1
+        done
 done
+
+exit $EXIT
