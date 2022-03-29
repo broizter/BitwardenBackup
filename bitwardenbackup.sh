4bbf5b3b52d2b27020ed936ada8d5a339f45c98b
broizter <39500356+broizter@users.noreply.github.com>
2022-03-29 20:35:48 +0200

    Encase some variables in quotes

--- bitwardenbackup.sh
+++ bitwardenbackup.sh
@@ -9,13 +9,13 @@ BW_BINARY=/path/to/bitwarden-cli/bin
 ############ END OF CONFIGURATION ################
 
 # Check if the output directory exists
-if ! [ -d $OUTPUTFOLDER ]; then
+if ! [ -d "$OUTPUTFOLDER" ]; then
         echo "Output folder doesn't exist." 1>&2
         exit 1
 fi
 
 # Check if the Bitwarden CLI binary exists
-if ! [ -f $BW_BINARY ]; then
+if ! [ -f "$BW_BINARY" ]; then
         echo "Bitwarden binary cannot be found. Check your settings." 1>&2
         exit 1
 fi
@@ -27,38 +27,38 @@ if ! command -v jq &> /dev/null; then
 fi
 
 # Check if the user is logged into Bitwarden
-if ! $BW_BINARY login --check &> /dev/null; then
+if ! "$BW_BINARY" login --check &> /dev/null; then
         echo "You need to login to BitWarden before running this script."
         exit 1
 fi
 
 # Variables used later in script. Not meant to be changed.
-SESSION=$($BW_BINARY unlock --raw $PASSWORD)
-PARENTS=$($BW_BINARY list items --session $SESSION | jq -r '.[] | select(.attachments) | .id')
-ATTACHFOLDER=$OUTPUTFOLDER/attachments/
-JSONFILE=$OUTPUTFOLDER/vault.json
+SESSION=$("$BW_BINARY" unlock --raw "$PASSWORD")
+PARENTS=$("$BW_BINARY" list items --session "$SESSION" | jq -r '.[] | select(.attachments) | .id')
+ATTACHFOLDER="$OUTPUTFOLDER"/attachments/
+JSONFILE="$OUTPUTFOLDER"/vault.json
 EXIT=0
 
 # Detect changes to your Bitwarden vault and export passwords to json file
-$BW_BINARY sync --session $SESSION || EXIT=$?
-$BW_BINARY export $PASSWORD --format json --session $SESSION --output $JSONFILE || EXIT=$?
+"$BW_BINARY" sync --session "$SESSION" || EXIT=$?
+"$BW_BINARY" export "$PASSWORD" --format json --session "$SESSION" --output "$JSONFILE" || EXIT=$?
 
 # Create folder where attachments end up
-if ! [ -d $ATTACHFOLDER ]; then
-        mkdir $ATTACHFOLDER || EXIT=$?
+if ! [ -d "$ATTACHFOLDER" ]; then
+        mkdir "$ATTACHFOLDER" || EXIT=$?
 fi
 
 # Goes through any item that contains attachments and downloads them
 for P in $PARENTS; do
-        if ! [ -d $ATTACHFOLDER/$P ]; then
-                mkdir $ATTACHFOLDER/$P || EXIT=$?
+        if ! [ -d "$ATTACHFOLDER"/"$P" ]; then
+                mkdir "$ATTACHFOLDER"/"$P" || EXIT=$?
         fi
-        ATTACH=$($BW_BINARY get item $P --session $SESSION | jq -r .attachments[].id)
+        ATTACH=$("$BW_BINARY" get item "$P" --session "$SESSION" | jq -r .attachments[].id)
         for A in $ATTACH; do
-                if ! [ -d $ATTACHFOLDER/$P/$A ]; then
-                        mkdir $ATTACHFOLDER/$P/$A || EXIT=$?
+                if ! [ -d "$ATTACHFOLDER"/"$P"/"$A" ]; then
+                        mkdir "$ATTACHFOLDER"/"$P"/"$A" || EXIT=$?
                 fi
-                $BW_BINARY get attachment $A --itemid $P --session $SESSION --output $ATTACHFOLDER/$P/$A/ || EXIT=$?
+                "$BW_BINARY" get attachment "$A" --itemid "$P" --session "$SESSION" --output "$ATTACHFOLDER"/"$P"/"$A"/ || EXIT=$?
         done
 done
 
